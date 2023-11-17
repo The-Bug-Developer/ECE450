edace4e74bb58c192db55724db7c020e8792e213
The-Bug-Developer <delu8015@vandals.uidaho.edu>
2023-11-17 11:18:37 -0800

    11-17-2023

--- FindFIR.py
+++ FindFIR.py
@@ -4,27 +4,33 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy import signal as sig
 from math import pi, exp, cos, sin, log, sqrt
+dt = .002
 NN = 100
 
 N2 = int(NN/2)
 x = np.zeros(NN)
 y = np.zeros(NN)
-dt = .002
+
 TT = np.linspace(0,dt*(NN-1),NN)
 DF = 1/(dt*NN)
 FF = np.linspace(0,DF*(NN-1),NN)
-f1 = 20
-f2 = 80
-f3 = 150
-freq1 =0.1
-freq2 = 1
-freq3 = 10
-x = 0.5*np.sin(freq1*TT) + 1*np.sin(freq2*TT) + 0.5*np.sin(freq3*TT)
-plt.figure(figsize=[5,5])
-plt.subplot(3,2,1)
+f1 = 30
+f2 = 60
+f3 = 90
+f4 = 120
+freq1 = 2*pi*f1
+freq2 = 2*pi*f2
+freq3 = 2*pi*f3
+freq4 = 2*pi*f4
+
+x = 2*np.sin(freq1*TT)
+x+= 2*np.sin(freq2*TT)
+x+= 2*np.sin(freq3*TT)
+x+= 2*np.sin(freq4*TT)
+plt.subplot(321)
 plt.plot(TT,x,'k')
 plt.axis([0,NN*dt,-2.5,2.5])
-#plt.text(5,-15,'$\phi$ = {}'.format(round(14,3)),fontsize=12)
+plt.text(5,-15,'$\phi$ = {}'.format(round(14,3)),fontsize=12)
 plt.title('Find_FIR')
 plt.ylabel('a). x[k]')
 plt.xlabel('T (sec)')
@@ -41,8 +47,13 @@ H = np.zeros(NN)
 #for n in range(4,16):
 # H[n] = exp(-.5*((n-4)/4)**2)
 """ Band pass """
-for n in range(N2):
-    H[n] = exp(-.5*((n-1)/4)**2)
+for n in range(12):
+    H[n] = exp(-.5*((n-12)/3)**2)
+for n in range(12,18):
+    H[n] = 1
+for n in range(18,N2):
+    H[n] = exp(-.5*((n-18)/3)**2)
+
 """ High pass """
 #for n in range(15):
 # H[n] = exp(-.5*((n-15)/4)**2)
@@ -52,48 +63,59 @@ for n in range(N2):
 for n in range(1,N2-1):
     H[NN-n] = H[n]
 Y = H*X
-plt.subplot(3,2,2)
+plt.subplot(322)
 plt.plot(FF,abs(X),'k',label='X')
 plt.plot(FF,H,'k--',label='H')
 plt.legend(loc='upper right')
 plt.ylabel('b). H(w),X(w)')
 plt.xlabel('Freq (Hz)')
 plt.grid()
-plt.axis([0,2,0,1.1])
-#plt.xticks([20,80,150])
+plt.axis([0,160,0,1.1])
+plt.xticks([20,80,150])
+
 h = np.fft.ifft(H)
+
 plt.subplot(323)
 plt.plot(h.real,'k')
 plt.xlabel('k')
 plt.ylabel('c). h[k]')
 plt.grid()
 plt.axis([0,NN,-.1,.2])
-M = 20
+
+M = 8
 hh = np.zeros(NN)
+
 """ Move the filter to the left side """
 for n in range(M):
     hh[n+M] = h[n].real
     hh[M-n] = hh[M+n]
-plt.subplot(3,2,4)
+
+plt.subplot(324)
 plt.plot(hh,'ok')
 plt.axis([0 ,2*M,-.1,.25])
 plt.xlabel('k')
 plt.ylabel('d). hh[k]')
 plt.grid()
+
 """ Convolve hh and x """
+
 yy=np.convolve(hh,x)
+
 y = np.zeros(NN)
 for n in range(NN):
     y[n] = yy[n+M]
-plt.subplot(3,2,5)
+
+plt.subplot(325)
 plt.plot(TT,y,'k')
 plt.ylabel('e). y[k]')
 plt.xlabel('T (sec)')
 plt.grid()
+
 Y = (1/NN)*np.fft.fft(y)
-plt.subplot(3,2,6)
+
+plt.subplot(326)
 plt.plot(FF,abs(Y),'k')
-plt.axis([0,200,-.1,.6])
+plt.axis([0,200,-.1,1])
 plt.xticks([0,20,80,150])
 plt.ylabel('f). Y[w]')
 plt.xlabel('Freq (Hz)')
