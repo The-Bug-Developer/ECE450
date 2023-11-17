3740e3b3095b094b0e4271eac7fa5fab57c49754
the-bug-developer <delu8015@vandals.uidaho.edu>
2023-11-16 19:16:53 -0800

    11-16-2023

--- Example_9_11.py
+++ Example_9_11.py
@@ -14,13 +14,13 @@ import cmath
 NN = 1000
 phi = np.linspace(0,2*pi,NN)
 dt = .01
-z = np.zeros(NN, dtype = np.complex)
-H = np.zeros(NN, dtype = np.complex)
+z = np.zeros(NN, dtype = complex)
+H = np.zeros(NN, dtype = complex)
 """---- One pole filter (Eq, 9.1.3) ------------"""
 for n in range(0,NN):
     z = cmath.exp(1j*phi[n])
     try:
-        H[n] = (0.002*z)/(z**2-1.97*z**1 + .97)
+        H[n] = (0.001*z)/(z**2-1.921*z + .922)
     except:
         print('woops')
 plt.subplot(211)
@@ -33,7 +33,7 @@ plt.axvline(5.7,color='k')
 plt.text(3,-15,'$\phi$ = {}'.format(round(5.7,3)),fontsize=12)
 plt.axvline(57,color='k')
 plt.text(30,-15,'$\phi$ = {}'.format(round(57,3)),fontsize=12)
-plt.title('Example_9-1-1')
+plt.title('Problem_9-1-1')
 plt.grid(which='both')
 aaa = np.angle(H)
 #for n in range(NN):
@@ -56,26 +56,26 @@ plt.xlabel('$\phi$ (degrees)')
 plt.savefig('H_zbode.png',dpi=300)
 plt.show()
 """This is the time-domain simulation of section 9.1.3"""
-MM = 500
+MM = 5000
 x = np.zeros(MM)
 y = np.zeros(MM)
 w1 = 5
 w2 = 100
 for k in range(1,MM):
     try:
-        x[k] = 1.*sin(w1*dt*k) + sin(w2*dt*k)
-        y[k] = 0.002*x[k]+1.97*y[k-1]-0.97*[k-2]
+        x[k] = 0.5*sin(5*dt*k) + sin(0.5*dt*k)
+        y[k] = 0.001*x[k-1]+1.921*y[k-1]-0.922*y[k-2]
     except:
-        print('woops')
-plt.subplot(211)
+        print(k)
+plt.subplot(2,1,1)
 plt.plot(x,'k')
 plt.ylabel('x[k]')
-plt.title('Example_9-1-1')
+plt.title('Problem_9-1-1')
 plt.grid()
-plt.subplot(212)
+plt.subplot(2,1,2)
 plt.plot(y,'k')
 plt.ylabel('y[k]')
-plt.axis([0,MM,-1.2,1.2])
+#plt.axis([0,MM,-1.2,1.2])
 plt.yticks([-1,-.7,0,.7,1])
 plt.grid()
 plt.savefig('Time_plot.png')
